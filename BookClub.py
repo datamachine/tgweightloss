d604cfad7158ba6cfd197f0d70d901e8d61c5ba5
Vincent Castellano <surye80@gmail.com>
2016-06-17 09:12:52 -0700

    Begin twx main loop

--- BookClub.py
+++ BookClub.py
@@ -1,6 +1,8 @@
 # Standard Library
 import logging
 import os.path
+from threading import Thread
+from time import sleep
 import configparser
 
 # 3rd Party Libraries
@@ -10,18 +12,32 @@ import twx.botapi
 import database
 
 
-class BookClubBot(object):
+class BookClubBot:
     def __init__(self, config):
         self.config = config
         self.database = database.Database()
         self.logger = logging.Logger("BookClubBot")
-        self.bot = twx.botapi.TelegramBot(self.config['BookClubBot']['bot_token'])
+        self.twx = twx.botapi.TelegramBot(self.config['BookClubBot']['bot_token'])
+        self.thread = Thread(target=self.main_loop)
 
     def run(self):
+        self.thread.run()
+        return self
+
+    def main_loop(self):
+        last_update = 0
+        while True:
+            # Process Telegram Events
+            updates = self.twx.get_updates(last_update).wait()
+            if updates:
+                for update in updates:
+                    if update:
+                        last_update = update.update_id + 1
+                        self.process_request(update)
+            sleep(.1)
+
+    def process_request(self, message):
         pass
-        # update_id = 0
-        # while True:
-        #     pass
 
 
 if __name__ == '__main__':
