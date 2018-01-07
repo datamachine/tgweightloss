307719714fc934dc074bdff909303e7e0fb75b60
Vincent Castellano <surye80@gmail.com>
2018-01-07 03:20:22 -0800

    gsheets/mfp code.

--- TGWeightLoss/WeightLoss.py
+++ TGWeightLoss/WeightLoss.py
@@ -7,12 +7,14 @@ from functools import wraps, partial
 import pytz
 import sqlalchemy.exc
 from dateutil.parser import parse as dtparse
+from datetime import date, timedelta
 from sqlalchemy import engine_from_config
 from twx import botapi
 from twx.botapi.helpers.update_loop import UpdateLoop, Permission
 
 import myfitnesspal
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 from TGWeightLoss.models import *
 
@@ -36,38 +38,35 @@ class WeightLossBot:
         self.bot.update_bot_info().wait()
 
         try:
-            self.mfp = GoodReadsClient(self.config['WeightLossBot']['goodreads.key'], self.config['BookClubBot']['goodreads.secret'])
+            self.mfp = myfitnesspal.Client(self.config['WeightLossBot']['myfitnesspal.user'], input("MFP Pass?"))
         except KeyError:
-            self.goodreads = None
+            self.mfp = None
+
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('gsheets_oauth.json', scope)
+        gc = gspread.authorize(credentials)
+        self.worksheet = gc.open_by_key(self.config['WeightLossBot']['gsheets.key'])
 
         self.update_loop = UpdateLoop(self.bot, self)
 
-        self.update_loop.register_inline_query_handler(self.inline_query)
-
         # region command registration
         # Admin Commands
-        self.update_loop.register_command(name='add_book', permission=Permission.Admin, function=self.add_book)
-        self.update_loop.register_command(name='start_book', permission=Permission.Admin, function=self.start_book)
-        self.update_loop.register_command(name='close_book', permission=Permission.Admin, function=self.close_book)
-        self.update_loop.register_command(name='register_ebook', permission=Permission.Admin, function=self.register_ebook)
-        self.update_loop.register_command(name='register_audiobook', permission=Permission.Admin, function=self.register_audiobook)
-        self.update_loop.register_command(name='set_deadline', permission=Permission.Admin, function=self.set_deadline)
+        self.update_loop.register_command(name='add_contest', permission=Permission.Admin, function=self.add_contest)
 
         # User Commands
-        self.update_loop.register_command(name='get_book', function=self.get_book)
-        self.update_loop.register_command(name='join_book', function=self.join_book)
-        self.update_loop.register_command(name='quit_book', function=self.quit_book)
-        self.update_loop.register_command(name='set_progress', function=self.set_progress)
-        self.update_loop.register_command(name='get_progress', function=self.get_progress)
-        self.update_loop.register_command(name='get_deadline', function=self.get_deadline)
+        # self.update_loop.register_command(name='join_book', function=self.join_contest)
+        # self.update_loop.register_command(name='set_progress', function=self.set_progress)
+        # self.update_loop.register_command(name='get_progress', function=self.get_progress)
+        # self.update_loop.register_command(name='get_deadline', function=self.get_deadline)
+        self.update_loop.register_command(name='mfp_summary', function=self.get_mfp_summary)
         # endregion
 
     # Admin Commands
-    # region add_book command
+    # region add_contest command
     @update_metadata
     def add_contest(self, msg, arguments):
         if arguments:
-            self.add_book__set_goodreads(msg, arguments)
+            self.add_contest__set_title(msg, arguments)
         else:
             query = self.bot.send_message(chat_id=msg.chat.id, text="Title of contest to add?",
                                           reply_markup=botapi.ForceReply.create(selective=True), reply_to_message_id=msg.message_id).join().result
@@ -76,195 +75,54 @@ class WeightLossBot:
     def add_contest__set_title(self, original_msg_id, msg):
         query = self.bot.send_message(chat_id=msg.chat.id, text="Start Date of Contest?",
                                       reply_markup=botapi.ForceReply.create(selective=True), reply_to_message_id=msg.message_id).join().result
-        self.update_loop.register_reply_watch(message=query, function=partial(self.add_contest__set_start_date, msg.text))
+        self.update_loop.register_reply_watch(message=query, function=partial(self.add_contest__set_date_start, msg.text))
 
-    def add_contest__set_start_date(self, book_title, msg):
-        author_name = msg.text
-
-        author = DBSession.query(Author).filter(Author.name == author_name).first()
-        if not author:
-            author = Author()
-            author.name = author_name
-            DBSession.add(author)
-
-        book = DBSession.query(Book).filter(Book.title == book_title).first()
-        if not book:
-            book = Book()
-            book.author = author
-            book.title = msg.text
-            DBSession.add(book)
-
-        assignment = DBSession.query(BookAssignment).filter(BookAssignment.book_id == book.id).filter(BookAssignment.chat_id == msg.chat.id).first()
-        if not assignment:
-            assignment = BookAssignment()
-            assignment.book = book
-            assignment.chat_id = msg.chat.id
-            DBSession.add(assignment)
-            DBSession.commit()
-            self.bot.send_message(chat_id=msg.chat.id, text=f"Added book to the group: {book.friendly_name}", reply_to_message_id=msg.message_id)
-
-    # endregion
-
-    # region set_deadline command
-    def _set_deadline(self, book_assignment_id, deadline, end_progress):
-        last_schedule = DBSession.query(BookSchedule).filter(BookSchedule.book_assignment_id == book_assignment_id).order_by(BookSchedule.due_date.desc()).first()
-
-        if last_schedule:
-            start = last_schedule.end
-        else:
-            start = 0
-
-        new_schedule = BookSchedule()
-        new_schedule.start = start
-        new_schedule.end = end_progress
-        new_schedule.due_date = deadline
-        new_schedule.book_assignment_id = book_assignment_id
-
-        DBSession.add(new_schedule)
-        DBSession.commit()
-
-    @update_metadata
-    def set_deadline(self, msg, arguments):
-        current_books = DBSession.query(BookAssignment).filter(BookAssignment.chat_id == msg.chat.id).filter(BookAssignment.current == True).all()
-
-        if len(current_books) == 0:
-            self.bot.send_message(chat_id=msg.chat.id, text="There are no current books to set the due date for.",
-                                  reply_to_message_id=msg.message_id)
-        elif len(current_books) == 1:
-            self.set_deadline__select_book(original_msg_id=msg.message_id, cbquery=None, data=current_books[0].id)
-        else:
-            reply = "Which book do you want to set the due date for?"
-
-            keyboard_rows = []
-            for book_assign in current_books:
-                keyboard_rows.append([botapi.InlineKeyboardButton(text=book_assign.book.friendly_name, callback_data=str(book_assign.id))])
-
-            keyboard = botapi.InlineKeyboardMarkup(inline_keyboard=keyboard_rows)
-
-            query = self.bot.send_message(chat_id=msg.chat.id, text=reply,
-                                          reply_markup=keyboard, reply_to_message_id=msg.message_id).join().result
-            self.update_loop.register_inline_reply(message=query, srcmsg=msg, function=partial(self.set_deadline__select_book, msg), permission=Permission.SameUser)
-
-    def set_deadline__select_book(self, original_msg_id, cbquery, data):
-        book = DBSession.query(BookAssignment).filter(BookAssignment.id == data).first()
-        query = self.bot.send_message(chat_id=book.chat_id, text="When is the next reading deadline?",
-                                      reply_markup=botapi.ForceReply.create(selective=True), reply_to_message_id=original_msg_id).join().result
-        self.update_loop.register_reply_watch(message=query, function=partial(self.set_deadline__select_deadline, data))
-
-    def set_deadline__select_deadline(self, book_assignment_id, msg):
+    def add_contest__set_date_start(self, contest_title, msg):
         try:
-            deadline = pytz.timezone("US/Pacific").localize(dtparse(msg.text))  # TODO: Proper timezone support #westcoastbestcoast
+            date_start = pytz.timezone("US/Pacific").localize(dtparse(msg.text))  # TODO: Proper timezone support #westcoastbestcoast
         except ValueError:
-            deadline = None
+            date_start = None
 
-        if deadline is not None:
-            query = self.bot.send_message(chat_id=msg.chat.id, text="What chapter is the next reading deadline through?",  # TODO: Support more than chapters in messages
-                                          reply_markup=botapi.ForceReply.create(selective=True), reply_to_message_id=msg.message_id).join().result
-            self.update_loop.register_reply_watch(message=query, function=partial(self.set_deadline__select_progress, deadline, book_assignment_id, query.message_id))
+        if date_start is not None:
+            query = self.bot.send_message(chat_id=msg.chat.id,
+                                          text="What chapter is the next reading deadline through?",
+                                          reply_markup=botapi.ForceReply.create(selective=True),
+                                          reply_to_message_id=msg.message_id).join().result
+            self.update_loop.register_reply_watch(message=query, function=partial(self.add_contest__set_date_end, contest_title, date_start, query.message_id))
         else:
-            # TODO: They are still sending more garbage..
-            self.bot.send_message(chat_id=msg.chat.id, text="Sorry, there was an error processing your answer", reply_to_message_id=msg.message_id)
+            # TODO: They are still sending more garbage.. Keep asking
+            query = self.bot.send_message(chat_id=msg.chat.id, text="Your date could not be processed, try again!",
+                                          reply_markup=botapi.ForceReply.create(selective=True),
+                                          reply_to_message_id=msg.message_id).join().result
+            self.update_loop.register_reply_watch(message=query,
+                                                  function=partial(self.add_contest__set_date_start, contest_title))
 
-    def set_deadline__select_progress(self, deadline, book_assignment_id, original_msg_id, msg):
-        book = DBSession.query(BookAssignment).filter(BookAssignment.id == book_assignment_id).first().book
+    def add_contest__set_date_end(self, contest_title, date_start, msg):
         try:
-            progress = int(msg.text)  # TODO: Another place to improve progress parsing.
+            date_end = pytz.timezone("US/Pacific").localize(dtparse(msg.text))  # TODO: Proper timezone support #westcoastbestcoast
         except ValueError:
-            progress = None
+            date_end = None
 
-        if progress is not None:
-            self.bot.send_message(chat_id=msg.chat.id,
-                                  text=f"Due date set for {book.friendly_name}: {deadline.strftime('%Y-%m-%d %I:%M %p %Z')}, to read through {progress}.")
-            self._set_deadline(book_assignment_id=book_assignment_id, end_progress=progress, deadline=deadline)
+        if date_end is not None:
+            contest = Contest()
+            contest.title = contest_title
+            contest.date_start = date_start
+            contest.date_end = date_end
+            DBSession.add(contest)
         else:
-            # TODO: They are still sending more garbage..
-            self.bot.send_message(chat_id=msg.chat.id, text="Sorry, there was an error processing your answer", reply_to_message_id=msg.message_id)
-
-    # endregion
-
-    # region start_book command
-    @update_metadata
-    def start_book(self, msg, arguments):
-        open_books = DBSession.query(BookAssignment).filter(BookAssignment.chat_id == msg.chat.id).filter(BookAssignment.done == False).filter(
-            BookAssignment.current == False).all()
-        current_book = DBSession.query(BookAssignment).filter(BookAssignment.chat_id == msg.chat.id).filter(BookAssignment.current == True).all()
-
-        if len(open_books) == 0:
-            self.bot.send_message(chat_id=msg.chat.id, text="There are no open books to start.",
-                                  reply_to_message_id=msg.message_id)
-        else:
-            reply = ""
-
-            if len(current_book) > 0:
-                reply += f"There are currently {len(current_book)} active books. \n"
-            reply += "Which book do you want to set as active?"
-
-            # TODO: Support a "More" button.
-            keyboard_rows = []
-            for book_assign in open_books:
-                keyboard_rows.append([botapi.InlineKeyboardButton(text=book_assign.book.friendly_name, callback_data=str(book_assign.id))])
-
-            keyboard = botapi.InlineKeyboardMarkup(inline_keyboard=keyboard_rows)
-
-            query = self.bot.send_message(chat_id=msg.chat.id, text=reply,
-                                          reply_markup=keyboard, reply_to_message_id=msg.message_id).join().result
-            self.update_loop.register_inline_reply(message=query, srcmsg=msg, function=self.start_book__select_book, permission=Permission.SameUser)
-
-    def start_book__select_book(self, cbquery, data):
-        assignment = DBSession.query(BookAssignment).filter(BookAssignment.id == int(data)).first()
-
-        if not assignment:
-            self.bot.send_message(chat_id=cbquery.message.chat.id, text="Error starting book, cannot find it in DB.")
-
-        assignment.current = True
-
-        DBSession.add(assignment)
-        DBSession.commit()
-
-        self.bot.edit_message_text(chat_id=cbquery.message.chat.id, message_id=cbquery.message.message_id, text=f"Starting book {assignment.book.friendly_name}.")
-
-    # endregion
-
-    # region close_book command
-    @update_metadata
-    def close_book(self, msg, arguments):
-        current_books = DBSession.query(BookAssignment).filter(BookAssignment.chat_id == msg.chat.id).filter(BookAssignment.done == False).filter(BookAssignment.current == True).all()
-
-        if len(current_books) == 0:
-            self.bot.send_message(chat_id=msg.chat.id, text="There are no active books to close.",
-                                  reply_to_message_id=msg.message_id)
-        else:
-            reply = "Which book do you want to close?"
-
-            # TODO: Support a "More" button.
-            keyboard_rows = []
-            for book_assign in current_books:
-                keyboard_rows.append([botapi.InlineKeyboardButton(text=book_assign.book.friendly_name, callback_data=str(book_assign.id))])
-
-            keyboard = botapi.InlineKeyboardMarkup(inline_keyboard=keyboard_rows)
-
-            query = self.bot.send_message(chat_id=msg.chat.id, text=reply,
-                                          reply_markup=keyboard, reply_to_message_id=msg.message_id).join().result
-            self.update_loop.register_inline_reply(message=query, srcmsg=msg, function=self.close_book__select_book, permission=Permission.SameUser)
-
-    def close_book__select_book(self, cbquery, data):
-        assignment = DBSession.query(BookAssignment).filter(BookAssignment.id == int(data)).first()
-
-        if not assignment:
-            self.bot.send_message(chat_id=cbquery.message.chat.id, text="Error closing book, cannot find it in DB.")
-
-        assignment.current = False
-
-        DBSession.add(assignment)
-        DBSession.commit()
-
-        self.bot.edit_message_text(chat_id=cbquery.message.chat.id, message_id=cbquery.message.message_id, text=f"Closing book {assignment.book.friendly_name}.")
+            # TODO: They are still sending more garbage.. Keep asking
+            query = self.bot.send_message(chat_id=msg.chat.id, text="Your date could not be processed, try again!",
+                                          reply_markup=botapi.ForceReply.create(selective=True),
+                                          reply_to_message_id=msg.message_id).join().result
+            self.update_loop.register_reply_watch(message=query,
+                                                  function=partial(self.add_contest__set_date_end, contest_title, date_start))
 
     # endregion
 
 
     # User Commands
     # region get_progress command
+    """
     def _send_progress(self, book_assignment_id, verbose, edit_message_id=None):
         assignment = DBSession.query(BookAssignment).filter(BookAssignment.id == book_assignment_id).one()
 
@@ -669,6 +527,47 @@ class WeightLossBot:
         #                                  results=results,
         #                                  cache_time=0,
         #                                  is_personal=True)
+"""
+
+    def get_mfp_summary(self, msg, arguments):
+        try:
+            summary_date = pytz.timezone("US/Pacific").localize(dtparse(arguments))  # TODO: Proper timezone support #westcoastbestcoast
+        except ValueError:
+            summary_date = date.today() - timedelta(1)
+
+        message = f"MFP Summary for {summary_date.strftime('%Y-%m-%d')}:\n\n"
+
+        for user in self._get_participants():
+            if user['mfp'].strip() == "":
+                message += f"{user['name']}: NO MFP SET\n"
+            else:
+                try:
+                    mfp_username = user['mfp'].split('/')[-1]
+                    data = self.mfp.get_date(summary_date, username=mfp_username).totals
+                    message += f"{user['name']}: Cals: {data['calories']}/{user['goal_calories']}, NetCarbs: {data['carbohydrates']-data['fiber']}/{user['goal_carbs']}, Fat: {data['fat']}/{user['goal_fat']}, Fat: {data['protein']}/{user['goal_protein']} \n"
+                except:
+                    message += f"{user['name']}: Nothing Logged, FOR SHAME\n"
+
+        self.bot.send_message(chat_id=msg.chat.id, text=message)
+
+    def _get_participants(self):
+        """
+        TODO: This is completely hardcoded to the DM contest... figure out how to make it more flexible
+        :return:
+        """
+        users = []
+        for row in range(2, 8):
+            users.append({
+                'name': self.worksheet.worksheet("Goals").cell(row, '1').value,
+                'telegram': self.worksheet.worksheet("Goals").cell(row, '12').value,
+                'mfp': self.worksheet.worksheet("Goals").cell(row, '13').value,
+                'goal_calories': self.worksheet.worksheet("Goals").cell(row, '8').value,
+                'goal_carbs': self.worksheet.worksheet("Goals").cell(row, '9').value,
+                'goal_fat': self.worksheet.worksheet("Goals").cell(row, '10').value,
+                'goal_protein': self.worksheet.worksheet("Goals").cell(row, '11').value,
+            })
+
+        return users
 
     def run(self):
         self.update_loop.run()  # Run update loop and register as handler
@@ -681,9 +580,9 @@ if __name__ == '__main__':
     configfile = configparser.ConfigParser()
     configfile.read('config.ini')
 
-    engine = engine_from_config(configfile['BookClubBot'], 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.create_all(engine)
+    # engine = engine_from_config(configfile['WeightLossBot'], 'sqlalchemy.')
+    # DBSession.configure(bind=engine)
+    # Base.metadata.create_all(engine)
 
-    mybot = BookClubBot(configfile)
+    mybot = WeightLossBot(configfile)
     mybot.run()
